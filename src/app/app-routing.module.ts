3a123595bc940c36463a28d8bbcb5f01aa2a6ee6
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2021-10-03 00:08:52 -0500

    Generated Main Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CommandsComponent } from './commands/commands.component';
+import { GetStartedComponent } from './get-started/get-started.component';
 import { MainComponent } from './main/main.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { ServersComponent } from './servers/servers.component';
 
 const routes: Routes = [
-    { path: '', component: MainComponent }
+  { path: '', component: MainComponent, data: {title: 'Main Page'} },
+  { path: 'get-started', component: GetStartedComponent, data: {title: 'Get Started'} },
+  { path: 'commands', component: CommandsComponent, data: {title: 'Commands'} },
+  { path: 'permissions', component: PermissionsComponent, data: {title: 'Permissions'} },
+  { path: 'servers', component: ServersComponent, data: {title: 'Servers'} },
+  { path: 'about', component: AboutComponent, data: {title: 'About'} },
+  { path: '**', component: PageNotFoundComponent, data: {title: '404 - Not Found'} }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { CommandsComponent } from './commands/commands.component';
+import { ServersComponent } from './servers/servers.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    GetStartedComponent,
+    CommandsComponent,
+    ServersComponent,
+    PermissionsComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
