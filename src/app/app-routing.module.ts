be122dcde7c46bf4989f78a68f47965ab9dd790d
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2021-10-02 20:24:15 -0500

    Generated main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-    { path: '/', component: AppComponent }
+    { path: '', component: MainComponent }
 ];
 
 @NgModule({
