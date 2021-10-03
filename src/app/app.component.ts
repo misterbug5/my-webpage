013adee55451510e27d29d731317346e8bbcda45
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2021-10-03 00:07:51 -0500

    Added Dinamic Titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'main page';
+export class AppComponent implements OnInit {
+
+  constructor(private titleService:Title, private router:Router, private activatedRoute:ActivatedRoute){}
+
+  ngOnInit(): void {
+    const appTitle = this.titleService.getTitle();
+    this.router
+      .events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => {
+          let child = this.activatedRoute.firstChild;
+          if (child) {
+            while (child.firstChild) {
+              child = child.firstChild;
+            }
+            if (child.snapshot.data['title']) {
+              return child.snapshot.data['title'];
+            }
+          }
+          return appTitle;
+        })
+      ).subscribe((ttl: string) => {
+        this.titleService.setTitle(ttl);
+      });
+  }
+
+  setDocTitle(title: string){
+    this.titleService.setTitle(title);
+  }
 }
